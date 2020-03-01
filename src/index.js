91a1da3324b473486736800b0bb9911b5959edb8
Job Ouddeken <jobouddeken@hotmail.com>
2020-03-01 15:39:21 +0100

    Finalize setup

--- src/index.js
+++ src/index.js
@@ -1,5 +1,26 @@
-import { AppRegistry } from 'react-native';
-import App from './App';
+import React from 'react';
+import {
+  AppRegistry,
+  StatusBar,
+  Text,
+  View,
+} from 'react-native';
 import { name as appName } from '../app.json';
+
+const App = () => (
+  <>
+    <StatusBar barStyle="dark-content" />
+    <View style={{
+      alignItems: 'center',
+      flex: 1,
+      height: '100%',
+      justifyContent: 'center',
+      width: '100%',
+    }}
+    >
+      <Text>{appName}</Text>
+    </View>
+  </>
+);
 
 AppRegistry.registerComponent(appName, () => App);
